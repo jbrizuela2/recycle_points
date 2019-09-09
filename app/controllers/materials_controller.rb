149169064f81d6be47095283f419fa3f56191c56
Juan Brizuela <juan.brizuela@michelada.io>
2019-09-09 12:54:18 -0500

    Added edit and delete options for materials

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,4 +1,6 @@
 class MaterialsController < ApplicationController
+  before_action :set_material, only: [:destroy, :edit, :update]
+
   def index
     @materials = Material.all
   end
@@ -18,9 +20,35 @@ class MaterialsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @material.update(material_params)
+      flash[:notice] = "Material actualizado exitosamente"
+      redirect_to recycle_points_path
+    else
+      flash[:alert] = "Hubo un error, y no se ha podido actualizar el material"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @material.destroy
+      flash[:notice] = "#{@material.name} eliminado correctamente"
+    else
+      flash[:alert] = "Algo ha salido mal, y no se ha podido eliminar el material"
+    end
+    redirect_to recycle_points_path
+  end
+
   private
 
   def material_params
     params.require(:material).permit(:name, :description)
+  end
+
+  def set_material
+    @material = Material.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :recycle_points
-  resources :materials
+  resources :materials, except: [:show]
 
   root to: "main#index"
 end
